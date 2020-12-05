634cb9769e70ad10caff6ee5ce9a64239e822ac6
dinubitingcatpetre <bitingcatmusic@gmail.com>
2020-12-05 11:19:25 +0200

    resolutionFHD

--- src/config.js
+++ src/config.js
@@ -8,8 +8,8 @@ export default {
   parent: 'game',
   backgroundColor: '#87ceeb',
   scale: {
-    width: 1280,
-    height: 720,
+    width: 1920,
+    height: 1080,
     mode: Phaser.Scale.FIT,
     autoCenter: Phaser.Scale.CENTER_BOTH,
   },
