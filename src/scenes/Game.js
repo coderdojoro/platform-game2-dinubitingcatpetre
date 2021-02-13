950043e1a87a8d74316f6b0ffab99e7fc1b30da4
Mihai Vasilache <mihai.vasilache@edenred.com>
2021-02-13 13:02:16 +0200

    tiles

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -12,6 +12,12 @@ class Game extends Phaser.Scene {
     this.load.spritesheet('walk-spritesheet', 'assets/rogue/walk.png', { frameWidth: 171, frameHeight: 128 });
     this.load.spritesheet('jump-spritesheet', 'assets/rogue/jump.png', { frameWidth: 171, frameHeight: 128 });
     this.load.spritesheet('double-jump-spritesheet', 'assets/rogue/double-jump.png', { frameWidth: 171, frameHeight: 128 });
+
+    this.load.tilemapTiledJSON('level1-tilemap', 'assets/tilemap.json');
+
+    this.load.image('ground-image', 'assets/tiles/tiles.png ');
+    this.load.image('bush-image', 'assets/tiles/bush-and-trees.png');
+
   }
 
   create() {
@@ -49,7 +55,24 @@ class Game extends Phaser.Scene {
 
     let hero = new Hero(this, 400, 300);
 
+    this.map = this.make.tilemap({ key: 'level1-tilemap' });
+    this.groundTiles = this.map.addTilesetImage('ground', 'ground-image');
+    this.bushTiles = this.map.addTilesetImage('bush', 'bush-image');
 
+    this.map.createStaticLayer('background' /*layer name from json*/, [this.groundTiles, this.bushTiles]);
+    this.groundLayer = this.map.createStaticLayer('ground' /*layer name from json*/, this.groundTiles);
+    this.map.createStaticLayer('foreground' /*layer name from json*/, [this.groundTiles, this.bushTiles]);
+
+    this.children.moveTo(hero, this.children.getIndex(this.map.getLayer('ground').tilemapLayer));
+
+    this.physics.add.collider(hero, this.groundLayer);
+    this.groundLayer.setCollisionBetween(1, 150, true); //64
+
+    this.cameras.main.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
+    this.cameras.main.startFollow(hero);
+    this.physics.world.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
+    //ca să nu dea cu capul de cer
+    this.physics.world.setBoundsCollision(true, true, false, true);
 
   }
 
